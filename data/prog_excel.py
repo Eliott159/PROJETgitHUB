46b4b007813ac49f9e15352bd4ec61157e8ebdae
Eliott159 <eliott.marceau867@gmail.com>
2022-01-05 19:52:00 +0100

    programme extraction data excel v2

--- data/prog_excel.py
+++ data/prog_excel.py
@@ -5,8 +5,8 @@ import xlrd
 
 civilité=[]
 age=[]
-
-
+licence=[]
+cp=[]
 
 document = xlrd.open_workbook("exportADOC_2021-2022.xls")
 sheet=document.sheet_by_name("Détaillé")
@@ -32,16 +32,30 @@ mini6=age.count('6')
 mini7=age.count('7')
 mini8=age.count('8')
 mini=mini1+mini2+mini3+mini4+mini5+mini6+mini7+mini8
-
 print('nombre mini : ',mini)
 
 
+licence=sheet.col_values(3)
+
+c=licence.count("Compétition")
+l=licence.count("Loisir")
+print('nombre de licence compétition : ',c)
+print('nombre de licence loisir : ',l)
 
 
+cp=sheet.col_values(3)
 
-"""
-print(sheet.cell_value(2, 2))
-"""
+v=cp.count('VOUNEUIL SOUS BIARD')
+mo=cp.count('MONTREUIL BONNIN')
+p=cp.count('POITIERS')
+b=cp.count('BERUGES')
+mi=cp.count('MIGNE AUXANCES')
+print('nombre de licencié provenant de Vouneuil Sous Biard : ',v)
+print('nombre de licencié provenant de Montreuil Bonin : ',mo)
+print('nombre de licencié provenant de Poitiers : ',p)
+print('nombre de licencié provenant de Beruges : ',b)
+print('nombre de licencié provenant de Migné Auxances : ',mi)
+
 
 
 """civilité, age, pratique licence, niveau de jeu"""
