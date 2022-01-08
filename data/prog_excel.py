a55e299da0684da29075a4cdde6d5ad813ecd2e2
Eliott159 <eliott.marceau867@gmail.com>
2022-01-08 15:28:36 +0100

    prog extraction v3

--- data/prog_excel.py
+++ data/prog_excel.py
@@ -1,16 +1,19 @@
 #usr!/bin/env python3
 
 import xlrd
-
+import maptolib.pyplot as plt
 
 civilité=[]
 age=[]
 licence=[]
 cp=[]
-
+mini=0
+poussin=0
+junior=0
+adulte=0
 document = xlrd.open_workbook("exportADOC_2021-2022.xls")
 sheet=document.sheet_by_name("Détaillé")
-
+nbligne=sheet.nrows
 
 
 civilité=sheet.col_values(3)
@@ -19,31 +22,49 @@ m=civilité.count("Monsieur")
 ma=civilité.count("Madame")
 print('nombre femmes : ',ma)
 print('nombre hommes : ',m)
+print('')
 
 
-age=sheet.col_values(3)
-
-mini1=age.count('1')
-mini2=age.count('2')
-mini3=age.count('3')
-mini4=age.count('4')
-mini5=age.count('5')
-mini6=age.count('6')
-mini7=age.count('7')
-mini8=age.count('8')
-mini=mini1+mini2+mini3+mini4+mini5+mini6+mini7+mini8
+age=sheet.col_values(5)
+age.pop(0)
+age.pop(0)
+x=0
+a=len(age)
+while a>=0:
+	age.count(x)
+	if 0<x<7:
+		mini=mini+1
+	elif 7<=x<11:
+		poussin=poussin+1
+	elif 11<=x<=17:
+		junior=junior+1
+	elif 18<=x<=150:
+		adulte=adulte+1
+	else:
+		pass
+	n=0
+	x=x+1
+	a=a-1
 print('nombre mini : ',mini)
+print('nombre poussin : ',poussin)
+print('nombre juniors : ',junior)
+print('nombre adultes : ',adulte)
+print('')
 
 
-licence=sheet.col_values(3)
 
-c=licence.count("Compétition")
+licence=sheet.col_values(50)
+
+c=licence.count('Compétition')
 l=licence.count("Loisir")
+nonr=licence.count('N')
 print('nombre de licence compétition : ',c)
 print('nombre de licence loisir : ',l)
+print('non renseingé : ',nonr)
+print('')
 
 
-cp=sheet.col_values(3)
+cp=sheet.col_values(11)
 
 v=cp.count('VOUNEUIL SOUS BIARD')
 mo=cp.count('MONTREUIL BONNIN')
@@ -57,5 +78,3 @@ print('nombre de licencié provenant de Beruges : ',b)
 print('nombre de licencié provenant de Migné Auxances : ',mi)
 
 
-
-"""civilité, age, pratique licence, niveau de jeu"""
